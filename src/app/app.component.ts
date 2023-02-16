5822a11e4d4115ab4f34f9cbc00e57ddf3ea9c88
Graham <116264932+GrahamLy0ns@users.noreply.github.com>
2023-02-16 09:53:31 +0000

    commit 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from "./article/article.model";
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redditApp';
+  articles:Article[];
+  constructor(){
+    this.articles=[
+    new Article("Angular", "http://angular.io",5),
+    new Article("Google", "http://google.ie",3),
+    new Article("TikTok", "http://tiktok.com",1)
+
+    ];
+
+  }
   addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean{
+    this.articles.push(new Article(title.value, link.value, 1));
+    title.value='';
+    link.value='';
     console.log(`Adding Article Details: ${title.value} and adding Article Link: ${link.value}`);
     return false;
 
