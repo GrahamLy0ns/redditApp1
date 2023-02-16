b60d57de6ebd744e37e7bf53fd86c040eb05d1ec
Graham <116264932+GrahamLy0ns@users.noreply.github.com>
2023-02-16 10:22:22 +0000

    commit 6

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -20,4 +20,14 @@ export class Article{
     
       }
 
+      domain(): string {
+        try {
+          const link: string = this.link.split('//')[1];
+          return link.split('/')[0];
+        }catch (err){
+          return "";
+
+        }
+      }
+
 }
